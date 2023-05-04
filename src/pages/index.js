faa8f8165fecea50d14f80ae9326318f5d96b9f0
MrK619 <97443351+MrK619@users.noreply.github.com>
2023-05-05 00:49:08 +0530

    incomplete landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,90 @@
 import * as React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "../components/header";
-import { Link } from "gatsby";
+
 import * as index from "../styles/index.module.css";
-
- 
-
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <main>
-      <body
-        className={index.main}
-         
-      >
+      <body className={index.main}>
         <NavBar />
-        <h1>Karthik Ambani R</h1>
-        <p>
-          You can find me on GitHub:{" "}
-          <a href="https://github.com/MrK619">https://github.com/MrK619</a>{" "}
-        </p>
-        <Link to="/contact">Contact me</Link>
-        
+        <div className={index.section1}>
+          <h1>Hey I am Karthik</h1>
+          <h2>a web developer</h2>
+        </div>
+        <div className={index.wrapper}>
+          <h2 className={index.project}>These are some of my projects</h2>
+          <div className={index.section2}>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/rot">
+              <div className={index.grid1}>
+                <img
+                  alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>Rot13</h1>
+              </div>
+            </Link>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/qr-generator">
+              <div className={index.grid2}>
+                <img
+                alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>QR Generator</h1>
+              </div>
+            </Link>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/rot">
+              <div className={index.grid3}>
+                <img
+                alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>rot13</h1>
+              </div>
+            </Link>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/rot">
+              <div className={index.grid4}>
+                <img
+                alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>rot13</h1>
+              </div>
+            </Link>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/rot">
+              <div className={index.grid5}>
+                <img
+                alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>rot13</h1>
+              </div>
+            </Link>
+            <Link style={{ textDecoration: "none", color: "#F0F0F0" }} to="/rot">
+              <div className={index.grid6}>
+                <img
+                alt="dummy"
+                  src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                  width={100}
+                  height={20}
+                ></img>
+                <h1>rot13</h1>
+              </div>
+            </Link>
+          </div>
+        </div>
       </body>
     </main>
   );
